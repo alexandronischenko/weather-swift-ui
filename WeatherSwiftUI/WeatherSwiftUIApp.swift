72d1bee6f3cdab251e3c9fc6949c7c32f460be42
alexandronischenko <alexandr.onischenko.2003@gmail.com>
2022-06-22 12:40:33 +0300

    Initial Commit

---
+++ WeatherSwiftUI/WeatherSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSwiftUIApp.swift
+//  WeatherSwiftUI
+//
+//  Created by Alexandr Onischenko on 22.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
